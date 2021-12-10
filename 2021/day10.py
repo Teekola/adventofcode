0d716b398bd2c08f9e77bd8e1b11d786cf7382e6
Teemu Kerkola <tokerk@utu.fi>
2021-12-10 22:25:16 +0200

    Day 10 completed!

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,14 +1,5 @@
 from time import perf_counter as pfc
 
-
-"""
-Calculate the error score
-=> Calculate the sum of the syntax scores of corrupted lines
-=> find the syntax score for each corrupted line
-=> check if a line is corrupted and then calculate its score
-=> check if a line is corrupted and get its score based on the first illegal character
-"""
-
 def readInput(fileName: str) -> list:
 
     with open(fileName, 'r') as file:
@@ -33,11 +24,19 @@ def getCharacterScore(char: chr) -> int:
 
 
 def firstIllegalCharacter(line) -> chr:
+    # Use as a stack
+    expected = []
+
+    # Iterate through indexes from 0 to end
     for i in range(len(line)):
         if line[i] in [')', ']', '}', '>']:
-            # Go through characters before line[i]
-            for j in range(i, -1, -1):
-                print(i, j)
+            # If the expected closing bracket is current bracket, remove expected from the expected list, otherwise we have illegal bracket
+            if expected[-1] == line[i]:
+                expected.pop()
+            else: return line[i]
+        else:
+            # Add the current opening bracket's closer as expected bracket
+            expected.append(flipBracket(line[i]))
 
     return ''
 
@@ -61,7 +60,63 @@ def solvePuzzle(puzzleInput) -> int:
     return sum(error_chars)
 
 
+
+# Part 2
+
+def findCompletionString(line) -> str:
+    # Create a stack of expected closing brackets
+    expected = []
+
+    for i in range(len(line)):
+        if line[i] in [')', ']', '}', '>']:
+            # If the expected closing bracket is current bracket, remove expected from the expected list
+            if expected[-1] == line[i]:
+                expected.pop()
+        else:
+            # Add the current opening bracket's closer as expected bracket
+            expected.append(flipBracket(line[i]))
+            
+    # Return the expected ones in reverse order as a string
+    return "".join(expected)[::-1]
+
+def scoreCompletionString(completionString) -> int:
+    score = 0
+    for char in completionString:
+        score *= 5
+        score += [')', ']', '}', '>'].index(char) + 1
+    return score
+
+"""
+Find the completion string for each incomplete line and add them to a list
+score the completion strings in the list
+sort the scores
+return the middle score
+"""
+def solvePuzzle2(puzzleInput) -> int:
+    lines = readInput(puzzleInput)
+
+    # Remove incomplete lines
+    lines =  [line for line in lines if firstIllegalCharacter(line) == '']
+    
+    # Add completion strings of each line to a list
+    completion_strings = []
+    for line in lines:
+        completion_strings.append(findCompletionString(line))
+
+    # Score the completion strings in the list
+    completion_scores = [scoreCompletionString(completionString) for completionString in completion_strings]
+
+    # Sort the scores
+    completion_scores.sort()
+
+    return completion_scores[len(completion_scores) // 2]
+
+
 if __name__ == "__main__":
     start_time = pfc()
     print(solvePuzzle("input10.txt"))
+    print(pfc() - start_time)
+
+    start_time = pfc()
+    print(solvePuzzle2("input10.txt"))
     print(pfc() - start_time)
